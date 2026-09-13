ef7addc18288a4885d0811684a087289dc63274f
MadWizardArt <blharris55@gmail.com>
2026-09-12 21:30:34 -0400

    Show sold paintings profit counter in inventory

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,4 +1,13 @@
 import Campaigns from "../components/Campaigns";
+import SoldPaintingsCounter from "../components/SoldPaintingsCounter";
+
+export const dynamic = "force-dynamic";
+
 export default function Page() {
-  return <Campaigns mode="inventory" />;
+  return (
+    <>
+      <Campaigns mode="inventory" />
+      <SoldPaintingsCounter />
+    </>
+  );
 }
