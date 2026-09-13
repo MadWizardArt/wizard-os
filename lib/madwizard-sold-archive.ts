55940bbe978e682e74874cad9951d946aec50c89
MadWizardArt <blharris55@gmail.com>
2026-09-12 21:29:12 -0400

    Add MadWizardArt sold archive source data

---
+++ lib/madwizard-sold-archive.ts
@@ -0,0 +1,160 @@
+export type MadWizardSoldArchiveWork = {
+  projectId: string;
+  title: string;
+  priceCents: number;
+  thumbnail: string;
+  dimensions: string;
+  medium: string;
+  sourceUrl: string;
+};
+
+export const MADWIZARD_SOLD_ARCHIVE_PREFIX = "madwizard-sold-";
+
+export const MADWIZARD_SOLD_ARCHIVE: MadWizardSoldArchiveWork[] = [
+  {
+    projectId: "madwizard-sold-ocean-madness",
+    title: "Ocean Madness (Sunny Side Up)",
+    priceCents: 30000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429707715/Phanes+Shatters+Atlantis029-Edit-2.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "",
+    medium: "",
+    sourceUrl: "https://www.madwizardart.com/product/ocean-madness",
+  },
+  {
+    projectId: "madwizard-sold-cloud-nine",
+    title: "Cloud Nine",
+    priceCents: 30000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429706947/Cloud+9_1.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "",
+    medium: "",
+    sourceUrl: "https://www.madwizardart.com/product/cloud-nine",
+  },
+  {
+    projectId: "madwizard-sold-yggdrasil",
+    title: "Yggdrasil",
+    priceCents: 40000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429706689/Yggdrasil+Small.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "20 x 20",
+    medium: "Acrylic on Canvas",
+    sourceUrl: "https://www.madwizardart.com/product/yggdrasil-original-painting",
+  },
+  {
+    projectId: "madwizard-sold-the-empyrean",
+    title: "The Empyrean",
+    priceCents: 100000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429706446/Where+Sunflowers+Grow+Final.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "16 x 20",
+    medium: "Acrylic on Canvas paper",
+    sourceUrl: "https://www.madwizardart.com/product/the-empyrean",
+  },
+  {
+    projectId: "madwizard-sold-behemoth",
+    title: "Behemoth",
+    priceCents: 30000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429705411/Behemoth.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "16 x 20",
+    medium: "Acrylic on Canvas",
+    sourceUrl: "https://www.madwizardart.com/product/behemoth",
+  },
+  {
+    projectId: "madwizard-sold-pegasus",
+    title: "Pegasus",
+    priceCents: 35000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429704526/White+Horse+Edit.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "12 x 16",
+    medium: "Acrylic on Canvas paper",
+    sourceUrl: "https://www.madwizardart.com/product/pegasus-original-painting",
+  },
+  {
+    projectId: "madwizard-sold-aethon",
+    title: "Aethon",
+    priceCents: 50000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429654930/Highhorse+Final.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "18 x 24",
+    medium: "Acrylic Painting",
+    sourceUrl: "https://www.madwizardart.com/product/aethon-original-painting",
+  },
+  {
+    projectId: "madwizard-sold-quetzalcoatl",
+    title: "Quetzalcoatl",
+    priceCents: 40000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429655062/Quetzalcoatl.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "18 x 24",
+    medium: "Acrylic on Canvas",
+    sourceUrl: "https://www.madwizardart.com/product/quetzalcoatl",
+  },
+  {
+    projectId: "madwizard-sold-moonfall",
+    title: "Moonfall",
+    priceCents: 40000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429655128/Luna+Final.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "18 x 24",
+    medium: "Acrylic on Canvas",
+    sourceUrl: "https://www.madwizardart.com/product/moonfall-original-painting",
+  },
+  {
+    projectId: "madwizard-sold-black-diamond",
+    title: "Black Diamond",
+    priceCents: 40000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429655158/Black+Diamond+Small.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "18 x 24",
+    medium: "Acrylic on Canvas",
+    sourceUrl: "https://www.madwizardart.com/product/black-diamond-original-painting",
+  },
+  {
+    projectId: "madwizard-sold-the-spheres",
+    title: "The Spheres",
+    priceCents: 10000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429655338/The+Spheres.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "11 x 14",
+    medium: "Acrylic on Canvas",
+    sourceUrl: "https://www.madwizardart.com/product/the-spheres",
+  },
+  {
+    projectId: "madwizard-sold-wyrmwoods-cradle",
+    title: "Wyrmwood's Cradle",
+    priceCents: 70000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429654282/Wyrmwood_s+Cradle+Low+Res.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "",
+    medium: "",
+    sourceUrl: "https://www.madwizardart.com/product/wyrmwoods-cradle-original-painting",
+  },
+  {
+    projectId: "madwizard-sold-star-of-bethlehem",
+    title: "Star of Bethlehem",
+    priceCents: 40000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429655416/Bethlehem+Star+Small.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "",
+    medium: "",
+    sourceUrl: "https://www.madwizardart.com/product/star-of-bethlehem-original-painting",
+  },
+  {
+    projectId: "madwizard-sold-over-the-hills-and-far-away",
+    title: "Over the Hills & Far Away",
+    priceCents: 70000,
+    thumbnail:
+      "https://assets.bigcartel.com/product_images/429654222/Over+the+Hills+and+Far+Away.jpg?auto=format&fit=max&h=1200&w=1200",
+    dimensions: "30 x 24 inch",
+    medium: "Acrylic on Canvas",
+    sourceUrl:
+      "https://www.madwizardart.com/product/over-the-hills-and-far-away-original-painting",
+  },
+];
+
+export const MADWIZARD_SOLD_LISTED_VALUE_CENTS = MADWIZARD_SOLD_ARCHIVE.reduce(
+  (sum, work) => sum + work.priceCents,
+  0,
+);
